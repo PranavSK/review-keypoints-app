5fa17853e3b0da0f513982d0ac699aaad3aaf6a8
Pranav S Koundinya <pranavpg101@gmail.com>
2024-03-20 20:08:26 +0530

    Only use the zip maker.

--- forge.config.cjs
+++ forge.config.cjs
@@ -14,24 +14,7 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: "@electron-forge/maker-squirrel",
-      config: {},
-    },
-    {
       name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
-    },
-    {
-      name: "@electron-forge/maker-deb",
-      config: {
-        options: {
-          icon: path.join(__dirname, "./resources/icon.png"),
-        },
-      },
-    },
-    {
-      name: "@electron-forge/maker-rpm",
-      config: {},
     },
   ],
   publishers: [
